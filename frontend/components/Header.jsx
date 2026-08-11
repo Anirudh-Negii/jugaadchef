8bd9dcfba58057b2f25e7766e5c7beff3cbd6d72
Anirudh <anirudh.formal88@gmail.com>
2026-08-12 00:31:18 +0530

    Build navbar

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import { Cookie, Refrigerator } from "lucide-react";
+import UserDropdown from "./UserDropdown";
 
-const Header = () => {
+const Header = async () => {
+  const user = null; // Replace with actual user authentication logic
+
   return (
     <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-blur:bg-stone50/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between ">
-        Logo
-        <div>Nav Links</div>
+        <Link href={user ? "/dashboard" : "/"}>
+          <Image src="/logo.png" alt="Logo" width={120} height={48} className='w-30 h-auto'  />
+        </Link>
+        <div className="hidden md:flex items-center space-x-8 text-sm font-medium text-stone-600">
+          <Link href="/recipes" className='hover:text-orange-600 transition-colors flex gap-1.5 items-center'>
+            <Cookie className="w-4 h-4" />
+            My Recipes
+          </Link>
+          <Link href="/pantry" className='hover:text-orange-600 transition-colors flex gap-1.5 items-center'>
+            <Refrigerator className="w-4 h-4" />
+            My Pantry
+          </Link>
+        </div>
         <div className="flex items-center space-x-4">
           <Show when="signed-in">
-            <UserButton />
+            {/* Implement how to cook button */}
+            <UserDropdown />
           </Show>
           <Show when="signed-out">
             <SignInButton mode="modal">
@@ -22,7 +40,10 @@ const Header = () => {
               </Button>
             </SignInButton>
             <SignUpButton mode="modal">
-              <Button variant="primary" className="rounded-full px-6 cursor-pointer">
+              <Button
+                variant="primary"
+                className="rounded-full px-6 cursor-pointer"
+              >
                 Get Started
               </Button>
             </SignUpButton>
